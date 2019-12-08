0aad11bb781f6db128750ccedc3834f40687f797
stellie82 <kims82@gmail.com>
2019-12-07 18:32:02 -0800

    append list items and buttons on burger creations

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -1,47 +1,58 @@
-$(document).ready(function () {
+var burgersList = [];
 
-    var burgersList = [];
+function getBurgers() {
+    $.get("/api/burgers", function (data) {
+        burgersList = data;
+        console.log(burgersList);
+        for (i = 0; i < burgersList.length; i++) {
+            $(".todo-list").append("<div class=todo-items>"
+                + "<li>" + burgersList[i].burger_name + "</li>"
+                + "<button class='devoured'>Devour It!</button>"
+                + "</div>");
+        }
+    })
+};
 
-    function getBurgers() {
-        $.get("/api/burgers", function (data) {
-            burgersList = data;
-            console.log(burgersList);
-            for (i = 0; i < burgersList.length; i++) {
-                $(".todo-list").append("<li>" + burgersList[i] + "</li>");
-            }
-        })
+$(".devoured").on("click", function (event) {
+    console.log("I clicked the devoured button");
+    var id = $(this).data("id");
+    console.log(id);
+    // var burgerDevoured = $(this).data("isdevoured");
+    
+    var devouredState = {
+        id: id,
+        devoured: true
     };
 
-    $(".devoured").on("click", function (event) {
-        var burgerDevoured = $(this).data("isdevoured");
-        updateBurger(burgerDevoured);
+    console.log(devouredState);
+    updateBurger(devouredState);
+});
+
+function updateBurger(burgerState) {
+    $.ajax({
+        method: "PUT",
+        url: "/api/burgers/" + id,
+        data: burgerState
+    }).then(function () {
+        location.reload();
     });
+};
 
-    function updateBurger(burgerState) {
-        $.ajax({
-            method: "PUT",
-            url: "/api/burgers",
-            data: burgerState
-        }).then(function () {
-            location.reload();
-        });
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+
+    var newBurger = {
+        burger_name: $("input.burger").val()
     };
+    console.log(newBurger);
 
-    $("#submit").on("click", function (event) {
-        event.preventDefault();
-
-        var newBurger = {
-            burger_name: $("input.burger").val()
-        };
-        console.log(newBurger);
-
-        $.ajax({
-            type: "POST",
-            url: "/api/burgers",
-            data: newBurger
-        }).then(function () {
-            location.reload();
-            getBurgers();
-        });
+    $.ajax({
+        type: "POST",
+        url: "/api/burgers",
+        data: newBurger
+    }).then(function () {
+        location.reload();
     });
 });
+
+$(document).ready(getBurgers);
